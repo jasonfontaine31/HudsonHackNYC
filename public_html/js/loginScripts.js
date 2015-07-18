53853f8505ed30f8c85a1c87d26304b0895d4e07
Matthew <Matthew@Matthew-PC>
2015-07-18 18:57:45 -0400

    Login Page for now

---
+++ public_html/js/loginScripts.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.fbAsyncInit = function() {
+FB.init({
+    appId      : '1153103148049536',
+    xfbml      : true,
+    version    : 'v2.3'
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+ 
